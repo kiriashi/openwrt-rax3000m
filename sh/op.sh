6262a6db6f9e29bf1fe4d8fef549cf35e27bfff8
雾岛听风 <78373893+kiriashi@users.noreply.github.com>
2026-09-12 19:16:57 +0800

    Update op.sh

--- sh/op.sh
+++ sh/op.sh
@@ -118,7 +118,7 @@ git clone https://github.com/sirpdboy/netspeedtest package/netspeedtest
 
 # homeproxy
 rm -rf package/porxy/luci-app-homeproxy
-git clone https://github.com/XiaoHaiSly/luci-app-homeproxy package/homeproxy
+git clone https://github.com/immortalwrt/homeproxy package/homeproxy
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
